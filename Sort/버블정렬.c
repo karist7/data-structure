b55aea3ea140b1beef837dbecc2cf51ffef57435
karist7 <49408613+karist7@users.noreply.github.com>
2022-12-26 16:35:47 +0900

    Update 버블정렬.c

--- Sort/버블정렬.c
+++ Sort/버블정렬.c
@@ -1,34 +1,34 @@
-//#include "Çì´õ.h"
-//
-//double compare;
-//double move;
-//
-//void bubble_sort(int arr[], int n) {
-//	for (int i = n-1; i > 0; i--) {
-//		
-//		for (int j = 0; j <i; j++) {
-//			compare++;
-//			move++;
-//			if (arr[j] > arr[j + 1]) {
-//				swap(&arr[j+1], & arr[j]);
-//				move+=3;
-//			}
-//		}
-//	}
-//}
-//
-//int main() {
-//	
-//	clock_t  start, end;
-//	start = clock();
-//	srand(time(NULL));
-//	int arr[MAX];
-//	create(arr, MAX);
-//	bubble_sort(arr, MAX);
-//	print(arr, MAX);
-//	end = clock();
-//	double dur = (double)((end - start)*1000) / CLOCKS_PER_SEC;
-//	printf("¼Ò¿ä½Ã°£: %.2lf\n", dur);
-//	
-//	printf("ÀÌµ¿È½¼ö: %lf, ºñ±³È½¼ö: %lf", move, compare);
-//}
+#include "í—¤ë”.h"
+
+double compare;
+double move;
+
+void bubble_sort(int arr[], int n) {
+	for (int i = n-1; i > 0; i--) {
+		
+		for (int j = 0; j <i; j++) {
+			compare++;
+			move++;
+			if (arr[j] > arr[j + 1]) {
+				swap(&arr[j+1], & arr[j]);
+				move+=3;
+			}
+		}
+	}
+}
+
+int main() {
+	
+	clock_t  start, end;
+	start = clock();
+	srand(time(NULL));
+	int arr[MAX];
+	create(arr, MAX);
+	bubble_sort(arr, MAX);
+	print(arr, MAX);
+	end = clock();
+	double dur = (double)((end - start)*1000) / CLOCKS_PER_SEC;
+	printf("ì†Œìš”ì‹œê°„: %.2lf\n", dur);
+	
+	printf("ì´ë™íšŸìˆ˜: %lf, ë¹„êµíšŸìˆ˜: %lf", move, compare);
+}
