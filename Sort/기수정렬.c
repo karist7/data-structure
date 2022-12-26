f3e2a57e62365df168e797dc35c647543c7e85b6
karist7 <49408613+karist7@users.noreply.github.com>
2022-12-26 16:38:56 +0900

    Update 기수정렬.c

--- Sort/기수정렬.c
+++ Sort/기수정렬.c
@@ -1,64 +1,64 @@
-//#include "Çì´õ.h"
-//
-//#define MAX_QUEUE_SIZE 100
-//typedef int element;
-//
-//typedef struct {
-//	element data[MAX_QUEUE_SIZE];
-//	int front, rear;
-//}QueueType;
-//
-//void error(char* message) {
-//	fprintf(stderr, "%s\n", message);
-//	exit(1);
-//}
-//
-//void init_queue(QueueType* q) {
-//	q->front = q->rear = 0;
-//}
-//
-//void enqueue(QueueType* q, element item) {
-//	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
-//	q->data[q->rear] = item;
-//}
-//
-//element dequeue(QueueType* q) {
-//	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
-//	return q->data[q->front];
-//}
-//int is_empty(QueueType* q) {
-//	return q->front == q->rear;
-//}
-//
-//#define BUCKET 10
-//#define DIGITS 4
-//
-//void radix_sort(int list[], int n) {
-//	int i, b, d, factor = 1;
-//	QueueType queues[BUCKET];
-//	for (b = 0; b < BUCKET; b++)init_queue(&queues[b]);
-//
-//	for (d = 0; d < DIGITS; d++) {
-//		for (i = 0; i < n; i++) {
-//			enqueue(&queues[(list[i] / factor) % 10], list[i]);
-//		}
-//		for (b = i = 0; b < BUCKET; b++) {
-//			while (!is_empty(&queues[b]))
-//				list[i++] = dequeue(&queues[b]);
-//		}
-//		factor *= 10;
-//	}
-//}
-//#define SIZE 10
-//int main() {
-//	int list[SIZE];
-//	srand(time(NULL));
-//	
-//	for (int i = 0; i < SIZE; i++)
-//		list[i] = rand() % 100;
-//	
-//	radix_sort(list, SIZE);
-//	for (int i = 0; i < SIZE; i++)
-//		printf("%d ", list[i]);
-//	printf("\n");
-//}
+#include "í˜œë”.h"
+
+#define MAX_QUEUE_SIZE 100
+typedef int element;
+
+typedef struct {
+	element data[MAX_QUEUE_SIZE];
+	int front, rear;
+}QueueType;
+
+void error(char* message) {
+	fprintf(stderr, "%s\n", message);
+	exit(1);
+}
+
+void init_queue(QueueType* q) {
+	q->front = q->rear = 0;
+}
+
+void enqueue(QueueType* q, element item) {
+	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+	q->data[q->rear] = item;
+}
+
+element dequeue(QueueType* q) {
+	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+	return q->data[q->front];
+}
+int is_empty(QueueType* q) {
+	return q->front == q->rear;
+}
+
+#define BUCKET 10
+#define DIGITS 4
+
+void radix_sort(int list[], int n) {
+	int i, b, d, factor = 1;
+	QueueType queues[BUCKET];
+	for (b = 0; b < BUCKET; b++)init_queue(&queues[b]);
+
+	for (d = 0; d < DIGITS; d++) {
+		for (i = 0; i < n; i++) {
+			enqueue(&queues[(list[i] / factor) % 10], list[i]);
+		}
+		for (b = i = 0; b < BUCKET; b++) {
+			while (!is_empty(&queues[b]))
+				list[i++] = dequeue(&queues[b]);
+		}
+		factor *= 10;
+	}
+}
+#define SIZE 10
+int main() {
+	int list[SIZE];
+	srand(time(NULL));
+	
+	for (int i = 0; i < SIZE; i++)
+		list[i] = rand() % 100;
+	
+	radix_sort(list, SIZE);
+	for (int i = 0; i < SIZE; i++)
+		printf("%d ", list[i]);
+	printf("\n");
+}
