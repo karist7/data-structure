862d3d06f2a1348a8d73be802fc23255e278df14
karist7 <49408613+karist7@users.noreply.github.com>
2022-12-26 16:43:26 +0900

    Add files via upload

---
+++ Search/순차탐.c
@@ -0,0 +1,16 @@
+//#include "Çì´õ.h"
+//
+//int seq_search(int list[],int key, int low, int high) {
+//	for (int i = low; i < high; i++) {
+//		
+//		if (key == list[i])
+//			return i;
+//		
+//	}
+//	return -1;
+//}
+//
+//int main() {
+//	int list[6] = { 3,7,5,1,6,3 };
+//	printf("%d ", seq_search(list, 5, 0, 6));
+//}
